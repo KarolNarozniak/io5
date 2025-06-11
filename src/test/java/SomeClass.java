902298e8709bc3407f80f3a38c06232998969c62
Karol Narozniak <karol.narozniak@o2.pl>
2025-06-11 12:12:25 +0200

    Działaj

---
+++ src/test/java/SomeClass.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class SomeClass {
+
+    @Test
+    public void test1(){
+        int value=2;
+        int value2=3;
+        assert value+value2==5;
+    }
+    @Test
+    public void test2() {
+        int value = 4;
+        int value2 = 5;
+        assert value + value2 == 9;
+    }
+}
