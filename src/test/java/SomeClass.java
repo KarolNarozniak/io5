e9768129a784ca766c3339bd5126d2daa1cb62a8
doziXt <adam.roznowski3@gmail.com>
2025-06-17 20:56:40 +0200

    Update SomeClass.java

--- src/test/java/SomeClass.java
+++ src/test/java/SomeClass.java
@@ -1,17 +1,18 @@
 import org.junit.Test;
 
 public class SomeClass {
-::}{:12983yhshshisihq
+;;012::}{:12983yhshshisihq
     @Test
     public void test1(){
         int value=2;
         int value2=3;
         assert value+value2==5;
+    manifest
     }
     @Test
     public void test2() {
         int value = 4;
         int value2 = 5;
         assert value + value2 == 9;
-    }
-}
+    }sad java
+   }}}
